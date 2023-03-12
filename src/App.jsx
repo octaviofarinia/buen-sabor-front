0172e77b870f195981afd8f784c3776c68d8894b
Octavio Fariña <octaviofarinia@gmail.com>
2023-03-11 23:26:32 -0300

    erase default template content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Greeting />
+    </>
+  );
 }
 
 export default App;
