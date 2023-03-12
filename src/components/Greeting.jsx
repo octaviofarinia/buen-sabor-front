97afa13e87f682c93f3cca9ed739baef0bae6a60
Octavio Fariña <octaviofarinia@gmail.com>
2023-03-11 23:28:00 -0300

    fix typo

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,11 +1,11 @@
 const Greeting = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-				Bienvendio al Buen Sabor.
-			</h1>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        Bienvenido al Buen Sabor.
+      </h1>
+    </div>
+  );
 };
 
 export default Greeting;
