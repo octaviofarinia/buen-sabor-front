1ea4076c1e66325d96de912518f65a1fc02f0606
Octavio Fariña <octaviofarinia@gmail.com>
2023-03-19 12:31:22 -0300

    added Log In and Logout wiht Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import './App.css';
 import Greeting from './components/Greeting';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
       <Greeting />
-    </>
+      <div className="my-10 flex justify-evenly">
+        <LoginButton />
+        <LogoutButton />
+      </div>
+      <div className="flex justify-center">
+        <Profile />
+      </div>
+    </Auth0Provider>
   );
 }
 
