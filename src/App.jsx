f01e211bbbb3bf195f93b1e31c48254f43f4f46c
Octavio Fariña <octaviofarinia@gmail.com>
2023-03-19 22:25:03 -0300

    first attempt - not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
-import { Auth0Provider } from '@auth0/auth0-react';
 import './App.css';
+import Auth0ProviderWithHistory from './components/Auth0/Auth0ProviderWithHistory';
 import Greeting from './components/Greeting';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
+import UserList from './components/UserList';
 
 function App() {
   return (
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
+    <Auth0ProviderWithHistory>
       <Greeting />
       <div className="my-10 flex justify-evenly">
         <LoginButton />
         <LogoutButton />
       </div>
-      <div className="flex justify-center">
-        <Profile />
-      </div>
-    </Auth0Provider>
+      <Profile />
+      <br className="my-10" />
+      <UserList />
+    </Auth0ProviderWithHistory>
   );
 }
 
---
+++ src/components/UserList.jsx
@@ -0,0 +1,130 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import React, { useState } from 'react';
+
+const UserList = () => {
+  const [state, setState] = useState({
+    showResult: false,
+    apiMessage: '',
+    error: null,
+  });
+
+  const { getAccessTokenSilently, loginWithPopup, getAccessTokenWithPopup } =
+    useAuth0();
+
+  const handleConsent = async () => {
+    try {
+      await getAccessTokenWithPopup();
+      setState({
+        ...state,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+
+    await callApi();
+  };
+
+  const handleLoginAgain = async () => {
+    try {
+      await loginWithPopup();
+      setState({
+        ...state,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+
+    await callApi();
+  };
+
+  const callApi = async () => {
+    try {
+      const token = await getAccessTokenSilently({
+        audience: 'https://buensaborback.com',
+        scope: 'all',
+      });
+      console.log(token);
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_API_BASE_URL}/usuarios`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const responseData = await response.json();
+
+      setState({
+        ...state,
+        showResult: true,
+        apiMessage: responseData,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+  };
+
+  const handle = (e, fn) => {
+    e.preventDefault();
+    fn();
+  };
+
+  return (
+    <>
+      <div className="mb-5 flex-col text-center">
+        {state.error === 'consent_required' && (
+          <div>
+            You need to{' '}
+            <a href="#/" onClick={(e) => handle(e, handleConsent)}>
+              consent to get access to users api
+            </a>
+          </div>
+        )}
+
+        {state.error === 'login_required' && (
+          <div>
+            You need to{' '}
+            <a href="#/" onClick={(e) => handle(e, handleLoginAgain)}>
+              log in again
+            </a>
+          </div>
+        )}
+
+        <h1>External API</h1>
+        <p className="text-xl font-bold">
+          Ping an external API by clicking the button below.
+        </p>
+        <button
+          className="mt-5 rounded bg-green-500 py-2 px-4 font-bold text-white hover:bg-green-700"
+          onClick={callApi}
+          disabled={!import.meta.env.VITE_AUTH0_AUDIENCE}
+        >
+          Ping API
+        </button>
+      </div>
+
+      <div>
+        {state.showResult && (
+          <div data-testid="api-result">
+            <h6>Result</h6>
+            <span>{JSON.stringify(state.apiMessage, null, 2)}</span>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default UserList;
