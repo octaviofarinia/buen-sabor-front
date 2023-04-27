b9dab87d8d63e2a01ced810b75faf773a3d1bb71
Franco Minati <francominati2@gmail.com>
2023-04-27 02:05:23 -0300

    abm rubro avance

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import axios from 'axios';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import AbmTable from './components/ABM/AbmTable';
+import CargaDeRegistro from './components/ABM/CargaDeRegistro';
 import LoginButton from './components/Auth0/LoginButton';
 import LogoutButton from './components/Auth0/LogoutButton';
 import Profile from './components/Auth0/Profile';
@@ -7,23 +11,33 @@ import ProtectedApiCall from './components/TestApiCalls/ProtectedApiCall';
 import PublicApiCall from './components/TestApiCalls/PublicApiCall';
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const deleteRegister = async ({ id }) => {
+    axios.delete('http://localhost:5173/api/v1/rubros-articulos/' + id);
+  };
+  const updateRegister = async ({ id }) => {
+    axios.put('http://localhost:5173/api/v1/rubros-articulos/' + id);
+  };
+  const createRegister = async () => {
+    axios.post('http://localhost:5173/api/v1/rubros-articulos/simple-save');
+  };
+  const getCategories = async () => {
+    let res = await axios.get('https://localhost:3306/api/v1/rubros-articulos');
+    setCategories(res.data);
+  };
+  useEffect(() => {
+    getCategories();
+  }, []);
   return (
     <>
-      <div className="text-center">
-        <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          Bienvenido al Buen Sabor
-        </h1>
-      </div>
-      <div className="my-10 flex justify-evenly">
-        <LoginButton />
-        <LogoutButton />
-      </div>
-      <Profile />
-      <hr className="my-10" />
-      <div className="flex w-full flex-row justify-around">
-        <PublicApiCall />
-        <ProtectedApiCall />
-        <AdminApiCall />
+      <div className={'flex h-full w-full'}>
+        <Routes>
+          <Route
+            path="/admin/abm-rubro"
+            element={<AbmTable information={categories} deleteMethod={deleteRegister}/>}
+          />
+          <Route path="/carga_registro" element={<CargaDeRegistro saveMethod={createRegister}/>} />
+        </Routes>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Auth0ProviderWithHistory from './components/Auth0/Auth0ProviderWithHistory';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <BrowserRouter>
     <Auth0ProviderWithHistory>
       <App />
     </Auth0ProviderWithHistory>
+    </BrowserRouter>
   </React.StrictMode>
 );
