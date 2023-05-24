e6326113df31b19036b05312cc28ef76f1e5a301
Octavio Fariña <octaviofarinia@gmail.com>
2023-05-24 11:22:07 -0300

    cambio post register flow

--- src/views/CargaDomicilioView.tsx
+++ src/views/CargaDomicilioView.tsx
@@ -9,15 +9,16 @@ const CargaDomicilioView: React.FC = () => {
   const queryParams = new URLSearchParams(location.search);
   const isNew = queryParams.get('new') === 'true';
 
-  const { user, isAuthenticated } = useAuth0();
+  const { user } = useAuth0();
 
   useEffect(() => {
     if (isNew) {
       const sendUserData = async () => {
-        if (isAuthenticated && user) {
+        if (user) {
           try {
-            const response = await axios.post(`${backend_url}/api/v1/domicilios/prueba`, {
-              userId: user.sub,
+            const response = await axios.post(`${backend_url}/api/v1/usuarios/post_register_save`, {
+              identityProvider: user.sub?.split('|')[0],
+              auth0Identifier: user.sub?.split('|')[1],
               username: user.name,
             });
             console.log(response.data);
@@ -29,11 +30,10 @@ const CargaDomicilioView: React.FC = () => {
 
       sendUserData();
     }
-  }, [isAuthenticated, user]);
+  }, [user]);
 
   return (
     <>
-      <div>{isNew ? 'true' : 'false'}</div>
       <div>CargaDomicilioView</div>
     </>
   );
