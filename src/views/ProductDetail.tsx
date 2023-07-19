40e1680f44962f212c0fa45acc79f8f211a7e3d2
Franco Minati <francominati2@gmail.com>
2023-07-18 23:25:24 -0300

    fix color correcto en detalle producto bg

--- src/views/ProductDetail.tsx
+++ src/views/ProductDetail.tsx
@@ -31,12 +31,12 @@ export const ProductDetailView = () => {
     };
   }, [user]);
   return (
-    <div className="bg-white px-5 pb-6 dark:bg-neutral-800 ">
+    <div className="bg-neutral-100 px-5 pb-6 dark:bg-neutral-800 ">
       <section className="body-font overflow-hidden text-neutral-600 dark:text-white">
         <div className="container mx-auto py-24">
           <div className="mx-auto flex flex-wrap justify-center lg:w-4/5 ">
             <div className="mb-6 w-full lg:mb-0 lg:w-1/2 lg:py-6 lg:pr-10">
-              <h2 className="title-font text-sm tracking-widest text-neutral-500 dark:text-zinc-200">
+              <h2 className="title-font text-sm tracking-widest text-neutral-500 dark:text-neutral-200">
                 el buen sabor
               </h2>
               <h1 className="title-font mb-4 text-3xl font-medium text-neutral-900 dark:text-white">
@@ -49,19 +49,19 @@ export const ProductDetailView = () => {
               </div>
               <p className="mb-4 leading-relaxed">{producto.descripcion}</p>
               <div className="flex border-t border-neutral-200 py-2">
-                <span className="text-neutral-500 dark:text-zinc-200">Tiempo estimado</span>
-                <span className="ml-auto text-neutral-900 dark:text-zinc-100">
+                <span className="text-neutral-500 dark:text-neutral-200">Tiempo estimado</span>
+                <span className="ml-auto text-neutral-900 dark:text-neutral-100">
                   {producto.tiempoEstimadoCocina} minutos
                 </span>
               </div>
               <div className="flex border-y border-neutral-200 py-2">
-                <span className="text-neutral-500 dark:text-zinc-200">Precio Unitario</span>
-                <span className="ml-auto text-neutral-900 dark:text-zinc-100">
+                <span className="text-neutral-500 dark:text-neutral-200">Precio Unitario</span>
+                <span className="ml-auto text-neutral-900 dark:text-neutral-100">
                   ${producto.precioVenta}
                 </span>
               </div>
               <div className="flex justify-between pt-2">
-                <h5 className="title-font text-2xl font-medium text-neutral-900 dark:text-zinc-100">
+                <h5 className="title-font text-2xl font-medium text-neutral-900 dark:text-neutral-100">
                   ${producto.precioVenta}
                 </h5>
                 <Button
